50c91932677826d79cefc770f6144f799f36d26f
tongcb <tongcb@knownsec.com>
2023-11-12 00:18:54 +0800

    add banner

--- cmd/appfinger/main.go
+++ cmd/appfinger/main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"github.com/projectdiscovery/gologger"
 	"github.com/projectdiscovery/gologger/levels"
 	"github.com/tongchengbin/appfinger/pkg/runner"
 )
+
+const Banner = `  ______                       ________  __                                         
+ /      \                     |        \|  \                                        
+|  $$$$$$\  ______    ______  | $$$$$$$$ \$$ _______    ______    ______    ______  
+| $$__| $$ /      \  /      \ | $$__    |  \|       \  /      \  /      \  /      \ 
+| $$    $$|  $$$$$$\|  $$$$$$\| $$  \   | $$| $$$$$$$\|  $$$$$$\|  $$$$$$\|  $$$$$$\
+| $$$$$$$$| $$  | $$| $$  | $$| $$$$$   | $$| $$  | $$| $$  | $$| $$    $$| $$   \$$
+| $$  | $$| $$__/ $$| $$__/ $$| $$      | $$| $$  | $$| $$__| $$| $$$$$$$$| $$      
+| $$  | $$| $$    $$| $$    $$| $$      | $$| $$  | $$ \$$    $$ \$$     \| $$      
+ \$$   \$$| $$$$$$$ | $$$$$$$  \$$       \$$ \$$   \$$ _\$$$$$$$  \$$$$$$$ \$$      
+          | $$      | $$                              |  \__| $$                    
+          | $$      | $$                               \$$    $$                    
+           \$$       \$$                                \$$$$$$                     
+`
 
 func main() {
 	gologger.DefaultLogger.SetMaxLevel(levels.LevelWarning)
@@ -13,6 +28,7 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	fmt.Printf(Banner)
 	err = appRunner.Enumerate()
 	if err != nil {
 		panic(err)
