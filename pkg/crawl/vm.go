fc9dbc19968bbb93c5b091751e8c5a95c35f9c19
tongchengbin <58296672@qq.com>
2025-07-27 19:26:00 +0800

    feat: implement virtual machine for crawling web page and recover vm run

--- pkg/crawl/vm.go
+++ pkg/crawl/vm.go
@@ -2,15 +2,34 @@ package crawl
 
 import (
 	"fmt"
-	"github.com/robertkrimen/otto"
 	"time"
+
+	"github.com/robertkrimen/otto"
 )
 
-// 执行 JavaScript 脚本并设置超时时间
+// 默认JavaScript执行的堆栈深度限制
+var DefaultStackDepthLimit = 300
+
+// 执行 JavaScript 脚本并设置超时时间，并处理可能的panic
 func runScriptWithTimeout(vm *otto.Otto, script string, timeout time.Duration) error {
 	done := make(chan error, 1)
 
+	// 设置脚本执行的最大指令数，防止无限循环
+	vm.SetStackDepthLimit(DefaultStackDepthLimit) // 使用全局配置的堆栈深度限制
+
 	go func() {
+		defer func() {
+			if r := recover(); r != nil {
+				switch r := r.(type) {
+				case error:
+					done <- fmt.Errorf("script execution panicked: %v", r)
+				default:
+					done <- fmt.Errorf("script execution panicked: %v", r)
+				}
+			}
+		}()
+
+		// 尝试安全执行脚本
 		_, err := vm.Run(script)
 		done <- err
 	}()
